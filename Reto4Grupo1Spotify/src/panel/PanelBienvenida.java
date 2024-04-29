8782584b2786dd9eca2d8f939b426467b96d0dc7
GontzalBilbao <gontzal.bilbaoza@elorrieta-errekamari.com>
2024-04-29 20:18:30 +0200

    panel de bienvenida

--- Reto4Grupo1Spotify/src/panel/PanelBienvenida.java
+++ Reto4Grupo1Spotify/src/panel/PanelBienvenida.java
@@ -1,16 +1,40 @@
 package panel;
 
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
 
-public class PanelBienvenida extends JPanel {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import vista.VentanaPrincipal;
+
+public class PanelBienvenida extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Create the panel.
 	 */
-	public PanelBienvenida() {
+	public PanelBienvenida(VentanaPrincipal v) {
+		setSize(800, 600);
+		setBackground(Color.DARK_GRAY);
+		setLayout(null);
+		
+		JLabel lblBienvenida = new JLabel("BIENVENIDO A SPOTIFY");
+		lblBienvenida.setFont(new Font("Lucida Bright", Font.BOLD, 20));
+		lblBienvenida.setForeground(Color.GREEN);
+		lblBienvenida.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBienvenida.setBounds(270, 150, 260, 51);
+		add(lblBienvenida);
+		
+		JLabel lblIconoGrande = new JLabel("");
+		lblIconoGrande.setIcon(new ImageIcon("icono/spotifyicon.png"));
+		lblIconoGrande.setBounds(350, 200, 100, 100);
+		add(lblIconoGrande);
 
 	}
-
+	
+	
 }
--- Reto4Grupo1Spotify/src/vista/VentanaPrincipal.java
+++ Reto4Grupo1Spotify/src/vista/VentanaPrincipal.java
@@ -1,5 +1,48 @@
 package vista;
 
-public class VentanaPrincipal {
+
+import javax.swing.JFrame;
+
+import panel.PanelBienvenida;
+
+public class VentanaPrincipal extends JFrame {
+
+	public VentanaPrincipal() {
+		setSize(800, 600);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Spotify");
+		
+		
+	}
+
+	public void cambiarDePanel(int i) {
+
+		switch (i) {
+		case 0:
+			setContentPane(new PanelBienvenida(this));
+			break;
+		case 1:
+			
+			break;
+		case 2: 
+			
+			break;
+		case 3:
+			
+			break;
+			
+			
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		VentanaPrincipal v = new VentanaPrincipal();
+		v.setVisible(true);
+		v.cambiarDePanel(0);
+		
+	}
 
 }
