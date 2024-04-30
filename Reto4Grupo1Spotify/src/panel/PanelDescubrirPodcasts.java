6bcfa9e847a0c9dd65b7b44ada35ef026bc99b02
GontzalBilbao <gontzal.bilbaoza@elorrieta-errekamari.com>
2024-04-30 20:47:44 +0200

    AÑADIR LAS VENTANAS DE DESCUBRIR: MUSICA PODCAST PLAYLIS

--- Reto4Grupo1Spotify/src/panel/PanelDescubrirPodcasts.java
+++ Reto4Grupo1Spotify/src/panel/PanelDescubrirPodcasts.java
@@ -1,8 +1,15 @@
 package panel;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import vista.VentanaPrincipal;
 
@@ -17,6 +24,38 @@ public class PanelDescubrirPodcasts extends JPanel {
 		setSize(800, 600);
 		setBackground(Color.DARK_GRAY);
 		setLayout(null);
+		
+		JLabel lblLogin = new JLabel("DESCUBIR PODCASTS");
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setForeground(Color.GREEN);
+		lblLogin.setFont(new Font("Lucida Bright", Font.BOLD, 20));
+		lblLogin.setBounds(270, 100, 260, 51);
+		add(lblLogin);
+		
+		JLabel lblIconoGrande = new JLabel("");
+		lblIconoGrande.setIcon(new ImageIcon("icono/spotifyicon.png"));
+		lblIconoGrande.setBounds(0, 0, 100, 100);
+		add(lblIconoGrande);
+		
+		JButton btnAtras = new JButton("ATRAS");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				v.cambiarDePanel(3);
+			}
+		});
+		btnAtras.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnAtras.setBounds(200, 36, 90, 35);
+		add(btnAtras);
+		
+		JButton btnPerfil = new JButton("PERFIL");
+		btnPerfil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				v.cambiarDePanel(11);
+			}
+		});
+		btnPerfil.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnPerfil.setBounds(500, 36, 90, 35);
+		add(btnPerfil);
 
 
 	}
