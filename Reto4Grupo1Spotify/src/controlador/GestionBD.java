490d6d72b18b470b0918cd9573df9f421b3071a0
GontzalBilbao <gontzal.bilbaoza@elorrieta-errekamari.com>
2024-04-30 20:49:14 +0200

    ULTIMA ACTUALIZACION

--- Reto4Grupo1Spotify/src/controlador/GestionBD.java
+++ Reto4Grupo1Spotify/src/controlador/GestionBD.java
@@ -12,7 +12,7 @@ public class GestionBD {
 	}
 
 	public void iniciarConexion() {
-		System.out.println("Conectando.........");
+		System.out.println("Conectando..........");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conexion = DriverManager.getConnection("jdbc:mysql://localhost/gestiondecine", "root", "");
